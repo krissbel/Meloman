420cd27e83d82592777e2a9b25225111bc7a8c9f
krissbel <krissalexnat@yandex.ru>
2022-04-27 22:45:44 +0800

    update

--- src/main/java/Main.kt
+++ src/main/java/Main.kt
@@ -1,26 +1,37 @@
 const val ITEM_PRICE = 200
+var itemCount = 1
+var discount = 100
+const val MELOMAN: Boolean = false
+
+const val ITEM_PRICE2 = 200
+var itemCount2 = 4
+const val MELOMAN2: Boolean = true
+
+val itemPrice3 = 200
+var itemCount3 = 100
+const val MELOMAN3: Boolean = true
+
 
 fun main() {
-    var discountStart = 1_000
-    var itemCount = 7
-    var discount = 100
-    val meloman: Boolean = true
-    var totalPrice = itemCount * ITEM_PRICE
-    println("Сумма: $totalPrice")
-    if(totalPrice > discountStart)
-        totalPrice -= discount
-    println("Сумма с учетом скидки: $totalPrice")
-    println("-------------------------------")
+    discount(ITEM_PRICE, itemCount, MELOMAN)
+    println("------------------------")
+    discount(ITEM_PRICE2, itemCount2, MELOMAN2)
+    println("------------------------")
+    discount(itemPrice3, itemCount3, MELOMAN3)
+}
 
-    discountStart = 10_000
-    itemCount = 51
-    totalPrice = itemCount * ITEM_PRICE
+fun discount(itemPrice: Int, itemCount: Int, meloman: Boolean) {
+    var totalPrice = itemCount * itemPrice
     println("Сумма: $totalPrice")
-    if(totalPrice > discountStart)
-        discount = totalPrice/100 * 5
-    totalPrice -=  discount
-    println("Сумма с учетом скидки: $totalPrice")
-    if(meloman){
+    if (totalPrice in 1001..10000) {
+        totalPrice -= discount
+        println("Сумма с учетом скидки: $totalPrice")
+    } else if (totalPrice > 10000) {
+        discount = totalPrice / 100 * 5
+        totalPrice -= discount
+        println("Сумма с учетом скидки: $totalPrice")
+    }
+    if (meloman) {
         totalPrice -= totalPrice / 100
         println("Сумма со скидкой постоянного покупателя: $totalPrice")
     }
